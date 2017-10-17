389858078bc4f7a845895d4ca4f1559de2670941
Paul Brown <paul@redjamjar.net>
2017-10-17 15:21:01 +0100

    passing validations with module based configs

--- terraform/mgmt/services/jenkins.tf
+++ terraform/mgmt/services/jenkins.tf
@@ -1,16 +1,19 @@
 /* Jenkins Server */
 
+module "vpc" {
+  source = "../vpc/"
+}
+
+module "global" {
+  source = "../../global"
+}
+
 variable "key_name" {
   default = "Jenkins"
 }
-variable "profile" {
-  default = "pbrowndev"
-}
-
-# Configure the AWS Provider
 
 provider "aws" {
-  region   = "eu-west-2"
+  region   = "${var.aws_region}"
   profile  = "${var.profile}"
 }
 
@@ -30,42 +33,7 @@ data "aws_ami" "octo" {
   owners = ["105761026190"] # EE
 }
 
-resource "aws_security_group" "jenkins" {
-  name        = "octo-jenkins-sg"
-  description = "Jenkins and SSH traffic"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "Octo Jenkins SG"
-  }
-}
 
 # Create a web server
 resource "aws_instance" "jenkins" {
@@ -73,7 +41,8 @@ resource "aws_instance" "jenkins" {
   ami               = "${data.aws_ami.octo.id}"
   instance_type     = "t2.medium"
   key_name          = "${var.key_name}"
-  security_groups   = ["${aws_security_group.jenkins.name}"]
+  security_groups   = ["${module.global.jenkins_sg}"]
+  subnet_id         = "${module.vpc.private_subnet}"
 
   tags {
     Name = "octo-jenkins-{{timestamp}}"
